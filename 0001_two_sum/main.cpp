baad282b08a6e11fc9c386fb6e4d2d63baa648aa
ezmi234 <ezmirondeniku@gmail.com>
2025-10-13 12:56:44 +0200

    feat: add solution to 0001

---
+++ 0001_two_sum/main.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+#include <unordered_map>
+#include <vector>
+
+using namespace std;
+
+class Solution {
+public:
+  vector<int> twoSum(vector<int> &nums, int target) {
+    unordered_map<int, int> map;
+
+    for (int i = 0; i < nums.size(); i++) {
+      int complement = target - nums[i];
+
+      if (map.find(complement) != map.end()) {
+        return {map[complement], i};
+      }
+      map[nums[i]] = i;
+    }
+    return {};
+  }
+};
+
+int main(int argc, char *argv[]) {
+  Solution sol;
+  vector<int> nums = {2, 7, 11, 15};
+  int target = 9;
+  vector<int> result = sol.twoSum(nums, target);
+
+  if (!result.empty()) {
+    cout << "Indices: " << result[0] << ", " << result[1] << endl;
+  } else {
+    cout << "No solution found." << endl;
+  }
+
+  return 0;
+}
